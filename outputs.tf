a0aad35f702dc512968a2b73fa6cd20adad8d5ee
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-08-31 11:49:53 -0500

    initial checkin

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "arn" {
+  description = "The ARN of the created datasource."
+  value       = "${aws_appsync_datasource.lambda_datasource.arn}"
+}
+
+output "role_arn" {
+  description = "The Amazon Resource Name (ARN) specifying the datasource's role. "
+  value       = "${aws_iam_role.lambda_datasource_role.arn}"
+}
+
+output "role_id" {
+  description = "The id of the datasource's role."
+  value       = "${aws_appsync_datasource.lambda_datasource.id}"
+}
+
+output "role_name" {
+  description = "The name of the datasource's role."
+  value       = "${aws_iam_role.lambda_datasource_role.name}"
+}
+
+output "role_unique_id" {
+  description = "The stable and unique string identifying the datasource's role."
+  value       = "${aws_appsync_datasource.lambda_datasource.unique_id}"
+}
