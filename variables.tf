5e9b64495dce34b4d18057b27a90c1072af294dd
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-07-02 17:55:01 +0530

    tfupgrade 0.12

--- variables.tf
+++ variables.tf
@@ -1,25 +1,26 @@
 variable "api_id" {
   description = "The API ID for the GraphQL API for the DataSource."
-  type        = "string"
+  type        = string
 }
 
 variable "invoke_lambda_policy_arn" {
   description = "The ARN of the policy that allows for the invocation of the Lambda function."
-  type        = "string"
+  type        = string
 }
 
 variable "lambda_function_arn" {
   description = "The ARN for the Lambda function."
-  type        = "string"
+  type        = string
 }
 
 variable "name" {
   description = "TA user-supplied name for the DataSource."
-  type = "string"
+  type        = string
 }
 
 variable "role_name_prefix" {
   default     = ""
   description = "Prefix to attach to the role name."
-  type        = "string"
+  type        = string
 }
+
